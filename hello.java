2fbf297c0a61b596e88ace90fb8f4935ec5a7135
tongchunpan <1809872737@qq.com>
2022-10-14 16:13:41 +0800

    dev2

--- hello.java
+++ hello.java
@@ -6,5 +6,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("master1");
         System.out.println("dev1");
+        System.out.println("dev2");
     }
 }
